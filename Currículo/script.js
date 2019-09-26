4e18d99637dd62aa287305b55ae6dd50e4b79248
Gustavo Pereira da Fonseca <39752736+Gustavo053@users.noreply.github.com>
2019-09-26 11:11:05 -0300

    Currículo

---
+++ Currículo/script.js
@@ -0,0 +1,19 @@
+let nome = document.getElementById("nome");
+if (nome.value != null) {
+    let valorNome = nome.value;
+}
+
+let email = document.getElementById("email");
+if (email.value != null) {
+    let valorEmail = email.value;
+}
+
+let numero = document.getElementById("numero");
+if (numero.value != null) {
+    let valorNumero = numero.value;
+}
+
+let mensagem = document.getElementById("mensagem");
+if (mensagem.value != null) {
+    let valorMensagem = mensagem.value;
+}
